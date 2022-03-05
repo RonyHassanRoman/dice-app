6f515278f687bb901d3474ccb8a02d7eed0336c4
Rony <ronyhassanroman2@gmail.com>
2022-03-05 22:34:08 +0600

    Finishing

--- lib/home.dart
+++ lib/home.dart
@@ -2,18 +2,18 @@ import 'package:dice_app/row.dart';
 import 'package:flutter/material.dart';
 
 class Homee extends StatelessWidget {
-  const Homee({ Key? key }) : super(key: key);
+  const Homee({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.redAccent,
+      backgroundColor: Colors.redAccent[100],
       appBar: AppBar(
-        title:const Text('Dice App'),
+        backgroundColor: Colors.redAccent[100],
+        title: const Text('Dice App'),
         centerTitle: true,
       ),
       body: const Roww(),
-      
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     home: Homee(),
+    debugShowCheckedModeBanner: false,
   ));
 }
-
